eae48bbc55bfa74babcaa9add4abc9f054e0709a
PaulLandaeta <paulwilkerlf@gmail.com>
2019-05-15 17:32:18 -0400

    create project

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Avatar from './components/Avatar'
+import { Constants } from 'expo';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Avatar initials={'PL'} size={35} backgroundColor={'teal'} />
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch'
+  },
+});
